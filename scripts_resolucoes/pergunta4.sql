3bc09b926d815a4f93221425c43dc505705e703d
VictorVechi <victorhvechi@gmail.com>
2023-08-17 19:43:57 -0300

    criação do arquivo de resposta para a pergunta 4

---
+++ scripts_resolucoes/pergunta4.sql
@@ -0,0 +1,2 @@
+select T.estudante_id, nome, COUNT(T.estudante_id) as qtd_cursos
+from Turmas_Alunos T inner join Estudantes E on T.estudante_id = E.estudante_id group by estudante_id having qtd_cursos > 1;
