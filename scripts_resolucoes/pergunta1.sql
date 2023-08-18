bae21bfbd7ef055016a0850b67c97260dcddac85
IgorSPinto <igorpinto103@gmail.com>
2023-08-17 21:28:43 -0300

    fix: :bug: adicionado comentários nos scripts
    adicionando comentários aos scripts de resolução apara facilitar a compreensão

--- scripts_resolucoes/pergunta1.sql
+++ scripts_resolucoes/pergunta1.sql
@@ -1,3 +1,5 @@
+/*Script de reposta da pergunta 1 sobre o total de estudantes do curso*/
+
 SELECT sum(qtd) as quantidade_total_alunos from (
 	select count(estudante_id) as qtd from Estudantes group by estudante_id
 ) as alunos group by qtd;
--- scripts_resolucoes/pergunta4.sql
+++ scripts_resolucoes/pergunta4.sql
@@ -1,2 +1,4 @@
+/*Script de reposta da pergunta 4 sobre os estudantes que fazem mais de um curso na instituição*/
+
 select T.estudante_id, nome, COUNT(T.estudante_id) as qtd_cursos
 from Turmas_Alunos T inner join Estudantes E on T.estudante_id = E.estudante_id group by estudante_id having qtd_cursos > 1;
